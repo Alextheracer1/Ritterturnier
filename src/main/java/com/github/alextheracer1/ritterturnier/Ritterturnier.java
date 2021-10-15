752b2901998f7393b0d55dc1e7a15b62fa32c898
Alex <alex.ander.graf@icloud.com>
2021-10-15 16:20:36 +0200

    Added Icon to the GUI

--- src/main/java/com/github/alextheracer1/ritterturnier/Ritterturnier.java
+++ src/main/java/com/github/alextheracer1/ritterturnier/Ritterturnier.java
@@ -3,9 +3,12 @@ package com.github.alextheracer1.ritterturnier;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Ritterturnier extends Application {
     @Override
@@ -13,6 +16,10 @@ public class Ritterturnier extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Ritterturnier.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Ritterturnier");
+        InputStream stream = Ritterturnier.class.getResourceAsStream("icon.jpg");
+        if (stream != null) {
+            stage.getIcons().add(new Image(stream));
+        }
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
